d1b080ff351c67ad8a33a0658d0d2d5cba8f39e3
Patrick Burns <pburns87@live.com>
2024-05-17 21:32:00 -0400

    better structure

---
+++ amazon_jobs/__init__.py
@@ -0,0 +1,19 @@
+import pathlib
+from .. import load_yaml_config
+
+# CONSTANTS
+
+AMAZON_JOBS_DIR = pathlib.Path(__file__).resolve().parent
+
+# CONFIGURATION
+
+AMAZON_JOBS_CONFIG = load_yaml_config(AMAZON_JOBS_DIR / 'config.yml')
+
+if 'amazon' not in AMAZON_JOBS_CONFIG:
+    raise RuntimeError('Amazon configuration missing')
+
+if 'zip' not in AMAZON_JOBS_CONFIG['amazon']:
+    raise RuntimeError('Amazon configuration requires zip code')
+
+if all(k not in AMAZON_JOBS_CONFIG['amazon'] for k in ('distance', 'cities')):
+    raise RuntimeError('Amazon configuration requires search criteria')   
---
+++ amazon_jobs/__main__.py
@@ -0,0 +1,64 @@
+import re
+from collections import namedtuple
+from . import AMAZON_JOBS_CONFIG as CFG
+from ..alert import Pushover
+from ..browser import Browser
+
+# CONSTANTS
+
+AMAZON_JOBS_URL = (
+    'https://hiring.amazon.com/app#/jobSearch?query='
+    f'&postal={CFG["amazon"]["zip"]}'
+    '&locale=en-US'
+)
+
+# REGEX PATTERNS
+
+RE_LOCATION = re.compile((
+    r'Within (?P<miles>\d+(\.\d+)?) mi \| '
+    r'(?P<city>[^,]+), '
+    r'(?P<state>[A-Z]{2})'
+))
+
+# GLOBAL VARIABLES
+
+jobs = []
+JobItem = namedtuple('JobItem', ['miles', 'city', 'state'])
+
+# DEW IT!
+
+with Browser() as wb:
+    wb.get(AMAZON_JOBS_URL)
+
+    wb.jitter(3, 5) # Wait for page to finish loading. There is a cleaner way to handle this.
+
+    wb.clear_form_by_css('[data-test-id="consentBtn"]') # cookies
+    wb.clear_form_by_css('[data-test-id="sortCloseModal"]') # notifications
+
+    wb.jitter(3, 5) # Wait again...
+
+    for item in wb.find_elements_by_class('jobCardItem'):
+        location = wb.find_child_by_xpath(item, './div/div/div[2]/*[last()]/strong')
+
+        if match := RE_LOCATION.match(location.text):
+            job = JobItem(
+                float(match.group('miles')),
+                match.group('city'),
+                match.group('state')
+            )
+
+            # Efficiency is not my middle name
+            if job.miles < CFG['amazon'].get('distance', 0):
+                jobs.append(job)
+            elif job.city in CFG['amazon'].get('cities', []):
+                jobs.append(job)
+
+    if not jobs:
+        exit()
+
+    content = ''
+    for j in jobs:
+        content += f"✅ {j.city}, {j.state} ({j.miles} mi)\n"
+
+    with Pushover(CFG['pushover']['api_key'], CFG['pushover']['user_key']) as p:
+        p.message(content)
