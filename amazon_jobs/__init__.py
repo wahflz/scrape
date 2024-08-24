6bd3f7cba593a659762087df80a5fedf94a86852
Patrick <pburns87@live.com>
2024-08-24 19:46:25 -0400

    cleaned config

--- amazon_jobs/__init__.py
+++ amazon_jobs/__init__.py
@@ -16,8 +16,5 @@ if 'amazon' not in AMAZON_JOBS_CONFIG:
 if 'zips' not in AMAZON_JOBS_CONFIG['amazon']:
     raise RuntimeError('Amazon configuration requires a zip code')
 
-# if all(k not in AMAZON_JOBS_CONFIG['amazon'] for k in ('distance', 'cities')):
-#     raise RuntimeError('Amazon configuration requires search criteria')
-
 logger = logging.getLogger('Amazon_Jobs')
 logger.setLevel(logging.INFO)
