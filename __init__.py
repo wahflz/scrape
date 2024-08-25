d09b80152dec3904b9ad639dfad837d24c4d8aad
Patrick <pburns87@live.com>
2024-08-25 01:09:18 -0400

    simplified geckodriver location

--- __init__.py
+++ __init__.py
@@ -1,28 +1,24 @@
 import yaml
+import shutil
 import pathlib
 import platform
 
 # CONSTANTS
 
 APP_DIR = pathlib.Path(__file__).resolve().parent
-BIN_DIR = APP_DIR / 'bin'
-APP_BIN = None
+BIN_DIR = APP_DIR / 'geckodriver'
+APP_BIN = shutil.which('geckodriver')
 
-match platform.machine().lower():
-    case 'amd64':
-        BIN_DIR = BIN_DIR / 'amd64'
-    case 'x86_64':
-        BIN_DIR = BIN_DIR / 'amd64'
-    case _:
-        raise OSError('Unsupported architecture')
-    
-match platform.system().lower():
-    case 'windows':
-        APP_BIN = BIN_DIR / 'windows' / 'geckodriver.exe'
-    case 'linux':
-        APP_BIN = BIN_DIR / 'linux' / 'geckodriver'
-    case _:
-        raise OSError('Unsupported operating system')
+if APP_BIN is None:
+    match platform.system().lower():
+        case 'windows':
+            APP_BIN = BIN_DIR / 'windows' / 'geckodriver.exe'
+        case 'linux':
+            APP_BIN = BIN_DIR / 'linux' / 'geckodriver'
+        case _:
+            raise OSError('Unsupported operating system')
+
+    raise OSError('geckodriver bin must be in PATH or CWD')
 
 # FUNCTIONS
 
