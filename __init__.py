dd13695529d3d8af8930632f5910088f71ced48d
Patrick <pburns87@live.com>
2024-08-25 01:09:42 -0400

    removed old code. oops

--- __init__.py
+++ __init__.py
@@ -10,14 +10,6 @@ BIN_DIR = APP_DIR / 'geckodriver'
 APP_BIN = shutil.which('geckodriver')
 
 if APP_BIN is None:
-    match platform.system().lower():
-        case 'windows':
-            APP_BIN = BIN_DIR / 'windows' / 'geckodriver.exe'
-        case 'linux':
-            APP_BIN = BIN_DIR / 'linux' / 'geckodriver'
-        case _:
-            raise OSError('Unsupported operating system')
-
     raise OSError('geckodriver bin must be in PATH or CWD')
 
 # FUNCTIONS
